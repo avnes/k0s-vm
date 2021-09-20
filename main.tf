aa6be39dcc2a8bd6bc823bb9e3a6d9b775263c6c
Audun Nes <audun.nes@gmail.com>
2021-09-20 19:49:07 +0200

    Remove var.valyria_vm_iso_name that has a NULL value

--- main.tf
+++ main.tf
@@ -1,6 +1,5 @@
 module "valyria_vm" {
     source          = "github.com/avnes/valyria-vm"
     project_name    = var.valyria_vm_project_name
-    iso_name        = var.valyria_vm_iso_name
     nodes           = var.valyria_vm_nodes
 }
