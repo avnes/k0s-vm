69e228c1036a5b799757db53bcd8aa422c97062a
Audun Nes <audun.nes@gmail.com>
2021-10-05 22:14:56 +0200

    Simplify module output

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,16 @@
-output "valyria_vm_hostnames" {
-    value = module.valyria_vm.hostnames
+output "valyria_vm_network" {
+    value = module.valyria_vm.network
 }
 
 output "valyria_vm_ansible_password" {
     value     = module.valyria_vm.ansible_password
     sensitive = true
+}
+
+output "valyria_vm_ssh_private_key_filename" {
+    value     = module.valyria_vm.ssh_private_key_filename
+}
+
+output "valyria_vm_ssh_public_key_filename" {
+    value     = module.valyria_vm.ssh_public_key_filename
 }
