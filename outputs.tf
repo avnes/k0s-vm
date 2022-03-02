9657e16fad2b324f01b8c848d3db277211d65da6
Audun Nes <audun.nes@gmail.com>
2022-03-02 22:01:18 +0100

    Add ssh commands

--- outputs.tf
+++ outputs.tf
@@ -1,16 +1,20 @@
 output "valyria_vm_network" {
-    value = module.valyria_vm.network
+  value = module.valyria_vm.network
 }
 
 output "valyria_vm_ansible_password" {
-    value     = module.valyria_vm.ansible_password
-    sensitive = true
+  value     = module.valyria_vm.ansible_password
+  sensitive = true
 }
 
 output "valyria_vm_ssh_private_key_filename" {
-    value     = module.valyria_vm.ssh_private_key_filename
+  value = module.valyria_vm.ssh_private_key_filename
 }
 
 output "valyria_vm_ssh_public_key_filename" {
-    value     = module.valyria_vm.ssh_public_key_filename
+  value = module.valyria_vm.ssh_public_key_filename
+}
+
+output "valyria_vm_ssh_command" {
+  value = module.valyria_vm.ssh_command
 }
