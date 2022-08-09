55fea0897b3a47601e8a39cf3ed8dcf5da5f19f4
Audun V. Nes <audun.nes@gmail.com>
2022-08-09 22:45:09 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,4 @@ module "valyria_vm" {
     source                  = "github.com/avnes/terraform-libvirt-vm"
     project_name            = var.valyria_vm_project_name
     nodes                   = var.valyria_vm_nodes
-    cloud_image_base_url    = var.valyria_vm_cloud_image_base_url
-    cloud_image_name        = var.valyria_vm_cloud_image_name
 }
